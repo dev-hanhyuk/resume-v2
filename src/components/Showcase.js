e7432990ed341a2b891dc0f0cc10c8f6f05aca45
Han Hyuk Cho <han@Hans-MacBook-Air.local>
2017-02-27 00:50:08 -0500

    Carousel speed reduced

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -37,7 +37,7 @@ export default class extends Component {
       slidesToShow: 1,
       slidesToScroll: 1,
       autoplay: true,
-      autoplaySpeed: 3000,
+      autoplaySpeed: 7000,
       pauseOnHover: false,
       beforeChange: (current, next) => {
         this.displaySlideContent(next)
